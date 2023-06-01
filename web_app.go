1a11d2f630938cb991cee8cffa87ffb37aafd11d
fadia <fadia@XLIM05040.sic.univ-poitiers.fr>
2023-06-01 17:06:02 +0200

    Changed port in web_app.go

--- web_app.go
+++ web_app.go
@@ -9,7 +9,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello fadia u awesome again & again!")
 	})
-	err := http.ListenAndServe(":1026", nil)
+	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
 		panic(err)
 	}
